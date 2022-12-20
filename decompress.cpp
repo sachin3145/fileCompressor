27ef9436c8b8a53acad6261dfb0012951a937d55
unknown <sachin3145@outlook.com>
2022-12-20 19:55:10 +0530

    added file to be used to decompress compressed files

---
+++ decompress.cpp
@@ -0,0 +1,14 @@
+#include "huffman.cpp"
+using namespace std;
+int main(int argc, char* argv[]) {
+    if (argc != 3) {
+        displayr("ERROR: Failed to detect Files\n");
+		exit(1);
+	}
+    
+    huffman file(argv[1], argv[2]);
+    file.decompress();
+    displayg("Decompressed successfully\n");
+
+    return 0;
+}
